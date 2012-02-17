57232ac4861b625c0bdfc222bb84ba655744bf7b
Jeff Larson <thejefflarson@gmail.com>
2012-02-17 17:18:25 -0500

    filter -> query

--- all_roads.c
+++ all_roads.c
@@ -10,10 +10,10 @@ main(){
   simplet_map_set_bounds(map, -585080.885134, 6849466.721081, 4161303.603672, 9587780.816356);
   simplet_map_set_size(map, 423, 260);
   simplet_layer_t *layer = simplet_map_add_layer(map, "PG:dbname=simple_tiles_roads");
-  simplet_filter_t *filter = simplet_layer_add_filter(layer, "select * from roads");
-  simplet_filter_add_style(filter, "stroke", "#11111166");
-  simplet_filter_add_style(filter, "line-join", "round");
-  simplet_filter_add_style(filter, "weight", "0.1");
+  simplet_query_t *query = simplet_layer_add_filter(layer, "select * from roads");
+  simplet_query_add_style(query, "stroke", "#11111166");
+  simplet_query_add_style(query, "line-join", "round");
+  simplet_query_add_style(query, "weight", "0.1");
 
   simplet_map_render_to_png(map, "./out.png");
   simplet_map_free(map);
